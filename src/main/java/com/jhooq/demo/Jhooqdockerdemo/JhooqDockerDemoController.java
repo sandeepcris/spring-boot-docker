ff3c24e02da187668cb8e9de1506c742f8defa96
sandeepcris <sandeep.cris@gmail.com>
2022-10-04 12:30:43 +0530

    V6

--- src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
+++ src/main/java/com/jhooq/demo/Jhooqdockerdemo/JhooqDockerDemoController.java
@@ -8,11 +8,11 @@ public class JhooqDockerDemoController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "New Version 1.1 Docker Demo - Hello Jhooq";
+        return "New Version 2.1 Docker Demo - Hello Jhooq";
     }
     @GetMapping("/test")
     public String test() {
-        return "This is test";
+        return "Ye hai sabse naya wala";
     }
 
 }
